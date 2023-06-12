ba16fc710eed9241dd3df3cb52e000dc2c949c8e
Valeriy Kudryk <58161114+wakud@users.noreply.github.com>
2023-06-12 10:31:51 +0300

    Добавьте файлы проекта.

---
+++ Rocky_Utility/WC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky_Utility 
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "kudrval@ukr.net";
+
+        public const string Success = "success";
+        public const string Error = "error";
+    }
+}

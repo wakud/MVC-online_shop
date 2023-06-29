3967d547504fe92b06a772ec47b29e9b8bc25047
Valeriy Kudryk <kudrval@ukr.net>
2023-06-29 17:34:55 +0300

    Refactoring

--- Rocky_DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Rocky_DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using Rocky_Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Rocky_Models;
 
 namespace Rocky_DataAccess.Repository.IRepository
 {
--- Rocky_DataAccess/Repository/IRepository/IRepository.cs
+++ Rocky_DataAccess/Repository/IRepository/IRepository.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rocky_DataAccess.Repository.IRepository
 {
